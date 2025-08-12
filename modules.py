a5351ea4e3e2050a6bab86e71c3beee43e0d4c91
halitklyc <halitenes.kalayci@gmail.com>
2025-08-12 10:33:10 +0300

    .

--- modules.py
+++ modules.py
@@ -1,7 +1,12 @@
 # 1- Built-in -> python'ın içine gömülü gelen modüller
 import math # built-in
+# 2- Kendi yazdığımız moduller
 import calculator # file-based modules
 # third-party
+import requests
 
 print(math.sqrt(64))
 print(calculator.calculate_tax(100))
+
+response = requests.get("https://google.com")
+print(response.status_code)
