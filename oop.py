685fda9a8995d196f0bd562e9a8d4d4bfbedd647
halitklyc <halitenes.kalayci@gmail.com>
2025-08-12 11:38:31 +0300

    .

--- oop.py
+++ oop.py
@@ -5,15 +5,18 @@ class Car():
         self.year = year
         print("Bir araba nesnesi oluştu.")
 
+    #def __init__(self,brand):
+       # self.brand = brand
+
     def start_rent(self): #Classın fonksiyonlarına ilk parametre python tarafından otomatik gönderilir.
         print(self.brand) # self -> classın kendisini ifade eder.
         print("Araç kiralama başlatılıyor")
     def stop_rent(self):
         print("Araç kiralama durduruluyor.")
 
-car1 = Car("Toyota",2025) # init fonk
+car1 = Car("Toyota",2025) # init fonk3
 car1.start_rent()
 car1.stop_rent()
 
-car2 = Car("Fiat", 2020) # init fonk
+car2 = Car("Fiat") # init fonk
 car2.start_rent()
