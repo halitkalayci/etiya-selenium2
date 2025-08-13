c2c017b34cdbab8e3c1054dc57913cbb316dca3e
halitklyc <halitenes.kalayci@gmail.com>
2025-08-13 13:35:57 +0300

    .

---
+++ project/pages/login_page.py
@@ -0,0 +1,22 @@
+#Giriş sayfasını sisteme tanıt.
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+class LoginPage():
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        #Sayfadaki elementleri ve özellikleri (içerik)
+        self.username_input = (By.ID, "user-name")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+        #
+    def load(self):
+        self.driver.get("https://saucedemo.com")
+        self.wait.until(expected_conditions.visibility_of_element_located(self.username_input))
+    def login(self,username,password):
+        self.wait.until(expected_conditions.visibility_of_element_located(self.username_input)).send_keys(username)
+        self.wait.until(expected_conditions.visibility_of_element_located(self.password_input)).send_keys(password)
+        self.wait.until(expected_conditions.element_to_be_clickable(self.login_button)).click()
+    def get_error_message(self):
+        pass
---
+++ project/tests/test_login.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Chrome
+from pages.login_page import LoginPage
+class TestLogin():
+    def test_login_valid(self):
+        driver = Chrome()
+        login_page = LoginPage(driver)
+        login_page.load()
+        login_page.login("standard_user","secret_sauce")
+        assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+    def test_login_invalid(self):
+        driver = Chrome()
+        login_page = LoginPage(driver)
+        login_page.load()
+        login_page.login("standard_user","secret_sauce123")
