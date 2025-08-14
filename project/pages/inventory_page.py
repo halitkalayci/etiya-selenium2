d3cd8426a1e487648721f4fdb321174566f9dd32
halitklyc <halitenes.kalayci@gmail.com>
2025-08-14 09:55:00 +0300

    .

---
+++ project/pages/inventory_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+class InventoryPage():
+    def __init__(self,driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+        self.inventory_container = (By.ID, "inventory_container")
+        self.products = (By.CLASS_NAME, "inventory_item")
+    
+    def load(self):
+        self.driver.get("https://www.saucedemo.com/inventory.html")
+        self.wait.until(expected_conditions.visibility_of_element_located(self.inventory_container))
+    
+    def get_product_list(self):
+        container = self.wait.until(expected_conditions.visibility_of_element_located(self.inventory_container))
+        products = container.find_elements(*self.products)
+        return products
