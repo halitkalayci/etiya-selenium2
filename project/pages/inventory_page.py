d70aabe1d630de9f64274833590231ab3069afcd
halitklyc <halitenes.kalayci@gmail.com>
2025-08-14 13:48:29 +0300

    .

--- project/pages/inventory_page.py
+++ project/pages/inventory_page.py
@@ -10,6 +10,10 @@ class InventoryPage():
 
         self.inventory_container = (By.ID, "inventory_container")
         self.products = (By.CLASS_NAME, "inventory_item")
+        self.product_title = (By.CLASS_NAME, "inventory_item_name")
+        self.product_desc = (By.CLASS_NAME, "inventory_item_desc")
+        self.product_price = (By.CLASS_NAME, "inventory_item_price")
+        self.product_img_url = (By.CLASS_NAME, "inventory_item_img")
     
     def load(self,username="standard_user", password="secret_sauce"):
         login_page = LoginPage(self.driver)
@@ -23,3 +27,11 @@ class InventoryPage():
         products = container.find_elements(*self.products)
         return products
     
+    def get_product_title(self,product):
+        return product.find_element(*self.product_title).text
+    def get_product_price(self,product):
+        pass
+    def get_product_description(self,product):
+        pass
+    def get_product_image_url(self,product):
+        pass
--- project/tests/test_inventory.py
+++ project/tests/test_inventory.py
@@ -1,7 +1,16 @@
 from pages.inventory_page import InventoryPage
-from time import sleep
+from utils.data_helper import read_csv_data
 class TestInventory():
     def test_product_length(self,driver):
         inv_page = InventoryPage(driver)
         inv_page.load()
-        sleep(10)
+        products = inv_page.get_product_list()
+        assert len(products) == 6
+    def test_product_information(self,driver):
+        csv_data = read_csv_data(r"C:\Users\PC1\Desktop\Projects\Education\Python\etiya-selenium2\project\data\products.csv")
+        inv_page = InventoryPage(driver)
+        inv_page.load()
+        products = inv_page.get_product_list()
+        for product in products:
+            title = inv_page.get_product_title(product)
+            print(title)
